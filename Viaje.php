6ceb679b2e0bf30b51b8251b552061ebb8a568e6
MEdithR <m.edith.robles@gmail.com>
2024-04-07 22:30:56 -0300

    class viaje inicida

---
+++ Viaje.php
@@ -0,0 +1,51 @@
+<?php
+
+class Viaje{
+
+    private $codViaje;
+    private $destino;
+    private $cantMaxPasaj;
+    private $pasViaje;
+
+    Public function __construct($codVia,$dest,$cantMax,$pasajerosVi){
+
+        $this->codViaje=$codVia;
+        $this->destino=$dest;
+        $this->cantMaxPasaj=$cantMax;
+        $this->pasViaje=$pasajerosVi;
+
+    }
+
+    public function getCodViaje(){
+        return $this->codViaje;
+    }
+
+    public function getDestino(){
+      return  $this->destino;
+    }
+
+    public function getCantMaxPasaj(){
+       return $this->cantMaxPasaj;
+    }
+
+    public function getPasViaje(){
+         return $this->pasViaje;
+    }
+
+    public function setCodViaje($codVia){
+        $this->codViaje=$codVia;
+    }
+
+    public function setDestino($dest){
+        $this->destino=$dest;
+    }
+
+    public function setCantMaxPasaj($cantMax){
+        $this->cantMaxPasaj=$cantMax;
+    }
+
+    public function setPasViaje($pasajerosVi){
+        $this->pasViaje=$pasajerosVi;
+    }
+
+}
